3a7c26fde87f4f66bf681d32c6c680d3f5dffe62
Brian Adams <bpeteradams@gmail.com>
2021-03-13 17:52:00 -0500

    wrote logic to send us to the search results page

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ var formatDropdown = document.querySelector("#format");
 var formatValue = formatDropdown.value
 var requestUrl = `https://www.loc.gov/search/${formatValue}/?q=${searchQuery}&fo=json`
 
-function searchLibraryOfCongress(){
-    searchQuery = searchQueryBox.value;
-    formatValue = formatDropdown.value;
-    fetch(requestUrl)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data.results)
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+// function searchLibraryOfCongress(){
+//     searchQuery = searchQueryBox.value;
+//     formatValue = formatDropdown.value;
+//     fetch(requestUrl)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function(data){
+//         console.log(data.results)
+//     })
+//     .catch(function(error){
+//         console.log(error)
+//     })
+// }
+
+function searchSubmit(){
+    var formatValue = formatDropdown.value
+    var searchQuery = searchQueryBox.value
+    if(!searchQuery){
+        console.error("You Must Enter a Search Query");
+        return
+    }
+    var queryString = `./searchresults.html?q=${searchQuery}format=${formatValue}`
+    location.assign(queryString);
 }
 
-//https://www.loc.gov/search/{format}/?q={searchQuery}
+//https://www.loc.gov/search/{format}/?q={searchQuery}&fo=json
